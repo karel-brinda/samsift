092484ba93c955d30e92da9d76fa53015bd437f9
Karel Břinda <karel.brinda@gmail.com>
2017-09-01 17:32:14 -0400

    Use PySam file objects correctly

--- samsift/samsift.py
+++ samsift/samsift.py
@@ -33,63 +33,63 @@ def sam_sift(in_sam_fn, out_sam_fn, filter, code, dexpr, dtrig):
 	if in_sam_fn=='-':
 		info("Reading from standard input. Press Ctrl+D to finish reading or run '{} -h' for help.".format(PROGRAM))
 
-	in_sam=pysam.AlignmentFile(in_sam_fn, "rb") #check_sq=False)
-	#print("@PG", "ID:{}".format(PROGRAM), "PN:{}".format(PROGRAM), "VN:{}".format(VERSION), "CL:{}".format(" ".join(sys.argv)), sep="\t")
-	header=in_sam.header
+	with pysam.AlignmentFile(in_sam_fn, "rb") as in_sam: #check_sq=False)
+		#print("@PG", "ID:{}".format(PROGRAM), "PN:{}".format(PROGRAM), "VN:{}".format(VERSION), "CL:{}".format(" ".join(sys.argv)), sep="\t")
+		header=in_sam.header
 
-	pg={
-			"ID":PROGRAM,
-			"PN":PROGRAM,
-			"VN":VERSION,
-			"CL":" ".join(map(lambda x:"'{}'".format(x),sys.argv)),
-		}
+		pg={
+				"ID":PROGRAM,
+				"PN":PROGRAM,
+				"VN":VERSION,
+				"CL":" ".join(map(lambda x:"'{}'".format(x),sys.argv)),
+			}
 
-	try:
-		header['PG'].insert(0,pg)
-	except KeyError:
-		header['PG']=[pg]
+		try:
+			header['PG'].insert(0,pg)
+		except KeyError:
+			header['PG']=[pg]
 
-	out_sam = pysam.AlignmentFile(out_sam_fn, "w", header=header)
+		with pysam.AlignmentFile(out_sam_fn, "w", header=header) as out_sam:
 
-	nt,np,nf=0,0,0
-	for a in in_sam.fetch(until_eof=True):
-		nt+=1
-		vardict={
-				'a': a,
-				'QNAME': a.query_name,
-				'FLAG': a.flag,
-				'RNAME': a.reference_id,
-				'POS': a.reference_start+1,
-				'MAPQ': a.mapping_quality,
-				'CIGAR': a.cigarstring,
-				'RNEXT': a.next_reference_id,
-				'PNEXT': a.next_reference_start+1,
-				'TLEN': a.template_length,
-				'SEQ': a.query_sequence,
-				'QUAL': a.query_qualities,
-				}
-		vardict.update(a.get_tags())
-		p=eval(filter, vardict)
-		if dexpr != "":
-			trig=eval(str(dtrig), vardict)
-			if trig:
-				res=eval(dexpr, vardict)
-				print(a.query_name, bool(p), res, file=sys.stderr, sep="\t")
-		if p:
-			if code is not None:
-				vardict_new=vardict.copy()
-				exec(code, vardict)
-				for k,v in vardict.items():
-					if len(k)==2:
-						if isinstance(v, float):
-							# workaround (see "https://github.com/pysam-developers/pysam/issues/531")
-							a.set_tag(k, v, value_type="f")
-						else:
-							a.set_tag(k, v)
-			out_sam.write(a)
-			np+=1
-		else:
-			nf+=1
+			nt,np,nf=0,0,0
+			for a in in_sam.fetch(until_eof=True):
+				nt+=1
+				vardict={
+						'a': a,
+						'QNAME': a.query_name,
+						'FLAG': a.flag,
+						'RNAME': a.reference_id,
+						'POS': a.reference_start+1,
+						'MAPQ': a.mapping_quality,
+						'CIGAR': a.cigarstring,
+						'RNEXT': a.next_reference_id,
+						'PNEXT': a.next_reference_start+1,
+						'TLEN': a.template_length,
+						'SEQ': a.query_sequence,
+						'QUAL': a.query_qualities,
+						}
+				vardict.update(a.get_tags())
+				p=eval(filter, vardict)
+				if dexpr != "":
+					trig=eval(str(dtrig), vardict)
+					if trig:
+						res=eval(dexpr, vardict)
+						print(a.query_name, bool(p), res, file=sys.stderr, sep="\t")
+				if p:
+					if code is not None:
+						vardict_new=vardict.copy()
+						exec(code, vardict)
+						for k,v in vardict.items():
+							if len(k)==2:
+								if isinstance(v, float):
+									# workaround (see "https://github.com/pysam-developers/pysam/issues/531")
+									a.set_tag(k, v, value_type="f")
+								else:
+									a.set_tag(k, v)
+					out_sam.write(a)
+					np+=1
+				else:
+					nf+=1
 	info("Finishing. {} alignments processed. {} alignments passed. {} alignments filtered out.".format(nt, np, nf))
 
 def main():
